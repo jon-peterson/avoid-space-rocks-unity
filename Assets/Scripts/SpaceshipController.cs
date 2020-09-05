2fcc3d963513162738f2a93201e4aab079904a39
jon <jon@ideograph.com>
2020-09-05 11:38:15 -0500

    Basic spaceship rotate; pixel-perfect camera

---
+++ Assets/Scripts/SpaceshipController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipController : MonoBehaviour
+{
+    // Speed in degrees that the spaceship and rotate at once
+    [SerializeField]
+    private float RotateSpeed = 270.0f; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Change rotation around the Z axis
+        float rot = -Input.GetAxisRaw("Horizontal");
+        this.gameObject.transform.Rotate(0, 0, rot * RotateSpeed * Time.deltaTime);
+        
+        // Fuel burn
+        // if(Input.GetAxisRaw("Vertical"))
+    }
+}
