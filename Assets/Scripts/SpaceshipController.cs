eb7ede6a2a79503797ecdda27daaabfa3d01d163
jon <jon@ideograph.com>
2020-09-06 15:08:12 -0500

    Add wraparound movement behavior; tweak spaceship movement

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -2,25 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpaceshipController : MonoBehaviour
-{
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(WraparoundMovement))]    
+public class SpaceshipController : MonoBehaviour {
     // Speed in degrees that the spaceship and rotate at once
-    [SerializeField]
-    private float RotateSpeed = 270.0f; 
+    [SerializeField] private float rotateSpeed = 270.0f;
+
+    // Force added when you hit the fuel
+    [SerializeField] private float fuelBoost = 10.0f;
+
+    // The maximum speed that you can make your ship
+    [SerializeField] private float maxSpeed = 30.0f;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Rigidbody2D _rigidbody2D;
+    
+    void Start() {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody2D.drag = 0.2f;
     }
 
-    void Update()
-    {
+    void Update() {
         // Change rotation around the Z axis
         float rot = -Input.GetAxisRaw("Horizontal");
-        this.gameObject.transform.Rotate(0, 0, rot * RotateSpeed * Time.deltaTime);
-        
+        transform.Rotate(0, 0, rot * rotateSpeed * Time.deltaTime);
+
         // Fuel burn
-        // if(Input.GetAxisRaw("Vertical"))
+        if (Input.GetAxisRaw("Vertical") > 0) {
+            Vector2 forward = transform.TransformDirection(Vector3.right);
+            Vector2 velocity = _rigidbody2D.velocity + (forward.normalized * (fuelBoost * Time.deltaTime));
+            _rigidbody2D.velocity = Vector2.ClampMagnitude(velocity, maxSpeed);
+        }
     }
 }
---
+++ Assets/Scripts/WraparoundMovement.cs
@@ -0,0 +1,28 @@
+﻿// Copyright 2020 Ideograph LLC. All rights reserved.
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+// If something flies off to the right, it should appear on the left; off the top, appear on the bottom; etc.
+public class WraparoundMovement : MonoBehaviour {
+
+    public void Update() {
+
+        Vector3 spaceshipPosition = gameObject.transform.position;
+        Vector3 screenDimensions = Camera.current.ViewportToWorldPoint(new Vector3(1.0f, 1.0f, 0.0f));
+
+        float x = spaceshipPosition.x;
+        float y = spaceshipPosition.y;
+        if (spaceshipPosition.x > screenDimensions.x) {
+            x = -screenDimensions.x;
+        } else if (spaceshipPosition.x < -screenDimensions.x) {
+            x = screenDimensions.x;
+        }
+        if (spaceshipPosition.y > screenDimensions.y) {
+            y = -screenDimensions.y;
+        } else if (transform.position.y < -screenDimensions.y) {
+            y = screenDimensions.y;
+        }
+
+        gameObject.transform.position = new Vector3(x, y, spaceshipPosition.z);
+    }
+}
