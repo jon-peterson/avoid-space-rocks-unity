a9c139e4bd2e814965995bb8c0681667ff46dc6f
jon <jon@ideograph.com>
2020-10-06 20:01:14 -0500

    Fuel burning sound

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright 2020 Ideograph LLC. All rights reserved.
+
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(WraparoundMovement))]    
+[RequireComponent(typeof(WraparoundMovement))] 
 public class SpaceshipController : MonoBehaviour {
     // Speed in degrees that the spaceship and rotate at once
     [SerializeField] private float rotateSpeed = 270.0f;
@@ -15,12 +17,18 @@ public class SpaceshipController : MonoBehaviour {
     
     private Rigidbody2D _rigidbody2D;
     private Animator _animator;
-    private LevelController _levelController = null;
+    private AudioSource _audioSource;
+    private AudioClip _audioFuelBurn;
+    private LevelController _levelController;
+    private bool _fuelBurning;
 
     void Start() {
         _animator = GetComponent<Animator>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _levelController = Util.GetLevelController();
+        _audioSource = gameObject.AddComponent<AudioSource>();
+        _audioFuelBurn = Resources.Load<AudioClip>("Audio/fuel_burn");
+        _fuelBurning = false;
     }
 
     void Update() {
@@ -29,12 +37,24 @@ public class SpaceshipController : MonoBehaviour {
         transform.Rotate(0, 0, rot * rotateSpeed * Time.deltaTime);
 
         // Fuel burn
-        _animator.SetBool("fuelBurn", false);
         if (Input.GetAxisRaw("Vertical") > 0) {
-            _animator.SetBool("fuelBurn", true);
             Vector2 forward = transform.TransformDirection(Vector3.right);
             Vector2 velocity = _rigidbody2D.velocity + (forward.normalized * (fuelBoost * Time.deltaTime));
             _rigidbody2D.velocity = Vector2.ClampMagnitude(velocity, maxSpeed);
+            if (!_fuelBurning) {
+                // Just started burning fuel so set up the audio
+                _fuelBurning = true;
+                _animator.SetBool("fuelBurn", true);
+                _audioSource.clip = _audioFuelBurn;
+                _audioSource.loop = true;
+                _audioSource.Play();
+            }
+        } else {
+            if (_fuelBurning) {
+                _fuelBurning = false;
+                _animator.SetBool("fuelBurn", false);
+                _audioSource.Stop();
+            }
         }
 
         // Fire
