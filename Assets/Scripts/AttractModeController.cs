bff59dfb0dd6f08b1e815fff92aec19cfb5f2ad4
jon <jon@ideograph.com>
2020-11-02 09:05:54 -0600

    Implement multi-screen attract mode

--- Assets/Scripts/AttractModeController.cs
+++ Assets/Scripts/AttractModeController.cs
@@ -1,9 +1,33 @@
 ﻿// Copyright 2020 Ideograph LLC. All rights reserved.
+
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class AttractModeController : MonoBehaviour
-{
+public class AttractModeController : MonoBehaviour {
+    private List<Transform> _textContainers;
+
+    void Start() {
+        _textContainers = new List<Transform>();
+        GameObject canvas = GameObject.Find("Canvas");
+        _textContainers.Add(canvas.transform.Find("Title"));
+        _textContainers.Add(canvas.transform.Find("Instructions"));
+        _textContainers.Add(canvas.transform.Find("Key Bindings"));
+        StartCoroutine(CycleThroughTitles());
+    }
+
+    private IEnumerator CycleThroughTitles() {
+        while (true) {
+            for (int i = 0; i < _textContainers.Count; i++) {
+                _textContainers[i].gameObject.SetActive(true);
+                yield return new WaitForSeconds(6.0f);
+                _textContainers[i].gameObject.SetActive(false);
+                yield return new WaitForSeconds(1.0f);
+            }
+        }
+    }
+
     // Start new game on any key
     void Update()
     {
