d27fa9962970110e89b2d48b27d1dd0605e4b961
jon <jon@ideograph.com>
2020-11-27 13:38:37 -0600

    Get the cognito user ID and use it to persist high scores

--- Assets/Scripts/HighScoreSceneController.cs
+++ Assets/Scripts/HighScoreSceneController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using Amazon;
 using Amazon.CognitoIdentity;
+using Amazon.CognitoIdentity.Model;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 using Amazon.DynamoDBv2.DocumentModel;
@@ -26,14 +27,6 @@ public class HighScoreSceneController : MonoBehaviour
     private String _gameTable = "avoid-space-rocks-player-dev";
     
     void Start() {
-        UnityInitializer.AttachToGameObject(this.gameObject);
-        AWSConfigs.HttpClient = AWSConfigs.HttpClientOption.UnityWebRequest;
-        _credentials = new CognitoAWSCredentials(_identityPoolId, RegionEndpoint.USEast2);
-
-        AmazonDynamoDBConfig ddbConfig = new AmazonDynamoDBConfig();
-        ddbConfig.RegionEndpoint = RegionEndpoint.USEast2;
-        _client = new AmazonDynamoDBClient(_credentials, ddbConfig);
-
         // Display the current high score
         GameObject canvas = GameObject.Find("Canvas");
         _scoreText = canvas.transform.Find("PlayerScore/FinalScore").gameObject.GetComponent<Text>();
@@ -41,7 +34,23 @@ public class HighScoreSceneController : MonoBehaviour
         _scoreText.text = score.ToString("#,##0");
         
         // Save the score that the player just made
-        SavePlayerScore();
+        UnityInitializer.AttachToGameObject(this.gameObject);
+        AWSConfigs.HttpClient = AWSConfigs.HttpClientOption.UnityWebRequest;
+        _credentials = new CognitoAWSCredentials(_identityPoolId, RegionEndpoint.USEast2);
+        
+        var identityClient = new AmazonCognitoIdentityClient(_credentials, RegionEndpoint.USEast2);
+        GetIdRequest request = new GetIdRequest();
+        request.IdentityPoolId = _identityPoolId;
+        identityClient.GetIdAsync(request, (result) => {
+            if (result.Exception == null) {
+                AmazonDynamoDBConfig ddbConfig = new AmazonDynamoDBConfig();
+                ddbConfig.RegionEndpoint = RegionEndpoint.USEast2;
+                _client = new AmazonDynamoDBClient(_credentials, ddbConfig);
+                SavePlayerScore(result.Response.IdentityId);
+            } else {
+                Debug.LogError("Failed to get Cognito identity: " + result.Exception);
+            }
+        });
         
         // After five seconds go to the attract mode
         Invoke("ReturnToAttractMode", 5.0f);
@@ -50,9 +59,9 @@ public class HighScoreSceneController : MonoBehaviour
     /**
      * Save the score info from the game status object to Dynamo for this player
      */
-    private void SavePlayerScore() {
-        PlayerScore score = new PlayerScore(_gameStatus, "1", "JEP");
-        PersistScoreToCollection("1", score);
+    private void SavePlayerScore(string playerId) {
+        PlayerScore score = new PlayerScore(_gameStatus, playerId, "JEP");
+        PersistScoreToCollection(playerId, score);
         PersistScoreToCollection("high-scores", score);
     }
 
